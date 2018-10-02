599cc1044d16aa3719a1c8caa17eb571a26710f1
DawnOfTheAge <40657815+DawnOfTheAge@users.noreply.github.com>
2018-10-02 13:47:37 +0300

    Add files via upload

---
+++ Lists Comparer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> l2 = new List<int>() { 5, 6, 7, 8, 9 };
+
+            var any = l2.Where(item => l1.Contains(item));
+
+            foreach (var item in any)
+            {
+                Console.WriteLine((int)item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
